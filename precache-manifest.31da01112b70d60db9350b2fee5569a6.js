fcb759e07f46b35e710a4598ff0b545522b5bb52
dawkaa <dawkabaymbaa27@gmail.com>
2020-02-04 16:52:06 +0800

    Updates

---
+++ precache-manifest.31da01112b70d60db9350b2fee5569a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd3c1febe9e9b0820a1be307325456e",
+    "url": "/react-homework/index.html"
+  },
+  {
+    "revision": "ef5020b099f07b9ad1ea",
+    "url": "/react-homework/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0eafb91732686823c3d9",
+    "url": "/react-homework/static/js/2.3081a46e.chunk.js"
+  },
+  {
+    "revision": "bbdaf1213ff22469a27f8a3d64940aac",
+    "url": "/react-homework/static/js/2.3081a46e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5020b099f07b9ad1ea",
+    "url": "/react-homework/static/js/main.2e465893.chunk.js"
+  },
+  {
+    "revision": "44cc922138abd0d44d81",
+    "url": "/react-homework/static/js/runtime-main.f66fe986.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-homework/static/media/logo.5d5d9eef.svg"
+  }
+]);
